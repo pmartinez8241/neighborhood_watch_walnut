1e1d9bf97b398612ec3c852c6918de11136cd23c
PAM-MANJARO-80085 <pammanjaro@gmail.com>
2024-06-01 15:50:19 -0700

    ADDED HEADER AND FOOTER

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,23 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "WALNUT Neighborhood Watch",
-      home: HomePage(),
-    );
+        home: Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(30.0),
+            
+            
+            child:AppBar(
+              centerTitle: true,
+              title: const Text("WALNUT Neighborhood watch"),
+              backgroundColor: Colors.orange,
+              
+            
+            )),
+            body: HomePage(),
+            bottomNavigationBar:
+              BottomNavigationBar(items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: Icon(Icons.camera), label: "Camera"),
+              BottomNavigationBarItem(icon: Icon(Icons.money), label: "Money")
+            ])));
   }
 }
