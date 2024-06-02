98d6697c63f27ee63ba85ba8f161cdd2b55491af
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-01 18:30:30 -0700

    Removed map from home.dart and moved it to Walnut_Map.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:neighborhood_watch_walnut/pages/walnut_map.dart';
 
 class HomePage extends StatefulWidget {
   static const String route = '/';
@@ -18,22 +17,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        FlutterMap(
-          options: MapOptions(
-              initialCenter: LatLng(34.04208, -117.84642),
-              initialZoom: 16,
-              cameraConstraint: CameraConstraint.contain(
-                  bounds: LatLngBounds(const LatLng(34.003931, -117.891541),
-                      const LatLng(34.071926, -117.818928)))),
-          children: [
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'com.example.app',
-            )
-          ],
-        )
-      ]),
+      body: Stack(children: [WalnutMap()]),
     );
   }
 }
