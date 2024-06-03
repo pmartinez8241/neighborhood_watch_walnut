34ab7b29cac71f17fc44d7839d39ac5e59cf9f12
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-02 19:23:43 -0700

    started a class that returns a MarkerLayer that will make the map

---
+++ lib/map_layers/mark_layer.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapMarkerOverlay extends StatelessWidget {
+  const MapMarkerOverlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MobileLayerTransformer(
+        child: MarkerLayer(markers: [
+      Marker(
+        point: LatLng(34.04900, -117.850000),
+        width: 64,
+        height: 64,
+        alignment: Alignment.centerLeft,
+        child: ColoredBox(
+          color: Colors.lightBlue,
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Text('-->'),
+          ),
+        ),
+      ),
+      Marker(
+        point: LatLng(34.051926, -117.880000),
+        width: 64,
+        height: 64,
+        alignment: Alignment.centerRight,
+        child: ColoredBox(
+          color: Colors.pink,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('<--'),
+          ),
+        ),
+      ),
+      Marker(
+        point: LatLng(34.010000, -117.820000),
+        rotate: false,
+        child: ColoredBox(color: Colors.black),
+      ),
+    ]) // your child here
+        );
+  }
+}
