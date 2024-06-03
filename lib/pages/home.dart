82b11114635a59a922f5990b1cc4c00c577de906
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-02 19:24:36 -0700

    The Home Scaffold is now CONST

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,7 +16,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Stack(children: [WalnutMap()]),
     );
   }
