6242bdf4d03248f3ab21d87e58570a859c82c5f0
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-03 18:16:17 -0700

    Clicking the submit button now creates a marker on the Map Layer

--- lib/pages/possible_crime_form.dart
+++ lib/pages/possible_crime_form.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CriminalFormSubmission extends StatefulWidget {
-  const CriminalFormSubmission({super.key});
+  final String markerCoordinates;
+
+  CriminalFormSubmission({Key? key, required this.markerCoordinates});
 
   @override
   CriminalFormSubmissionState createState() {
@@ -21,6 +24,14 @@ class CriminalFormSubmissionState extends State<CriminalFormSubmission> {
             child: Column(
               children: <Widget>[
                 TextFormField(
+                  decoration: InputDecoration(labelText: "Location"),
+                  readOnly: true,
+                  initialValue: widget.markerCoordinates,
+                ),
+                TextFormField(
+                  maxLines: 10,
+                  decoration:
+                      const InputDecoration(labelText: "Description of Event"),
                   // The validator receives the text that the user has entered.
                   validator: (value) {
                     if (value == null || value.isEmpty) {
