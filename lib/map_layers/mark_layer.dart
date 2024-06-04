7c0736a589a8f5c6d3a7c022daa673fb37eaf654
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-03 18:15:28 -0700

    Clicking on map brings up a form, that when submitted marks the map!
    Need to find a way to update the Mark Layer so that the marker appears on submit

--- lib/map_layers/mark_layer.dart
+++ lib/map_layers/mark_layer.dart
@@ -2,14 +2,86 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
+class MapMarkerOverlay extends StatefulWidget {
+  List<Marker> markerList = [
+    Marker(
+      point: LatLng(34.04900, -117.850000),
+      width: 20,
+      height: 20,
+      alignment: Alignment.centerLeft,
+      child: ColoredBox(
+        color: Colors.lightBlue,
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Text('-->'),
+        ),
+      ),
+    )
+  ];
+
+  MapMarkerOverlay({super.key});
+
+  void submitNewMarker(double lat, double lon) {
+    markerList.add(Marker(
+      point: LatLng(lat, lon),
+      width: 20,
+      height: 20,
+      alignment: Alignment.centerLeft,
+      child: const ColoredBox(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Text('-->'),
+        ),
+      ),
+    ));
+  }
+
+  @override
+  State<MapMarkerOverlay> createState() => _MapMarkerOverlayState();
+}
+
+class _MapMarkerOverlayState extends State<MapMarkerOverlay> {
+  @override
+  Widget build(BuildContext context) {
+    return MobileLayerTransformer(
+        child: MarkerLayer(
+      rotate: false,
+      markers: widget.markerList,
+    ));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+
 class MapMarkerOverlay extends StatelessWidget {
-  const MapMarkerOverlay({super.key});
+  List<Marker> markerList = [];
+  MapMarkerOverlay({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MobileLayerTransformer(
-        child: MarkerLayer( rotate: false, markers: [
+        child: MarkerLayer(
+      rotate: false,
+      markers: markerList,
+    ) // your child here
+        );
+  }
+}
 
+
+
+
+markers: [
       Marker(
         point: LatLng(34.04900, -117.850000),
         width: 64,
@@ -39,9 +111,6 @@ class MapMarkerOverlay extends StatelessWidget {
       Marker(
         point: LatLng(34.010000, -117.820000),
         rotate: false,
-        child: ColoredBox(color: Colors.black),
-      ),
-    ]) // your child here
-        );
-  }
-}
+        child: ColoredBox(color: Colors.deepPurple),
+      ),]
+*/
