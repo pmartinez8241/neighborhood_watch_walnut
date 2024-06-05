fd3c46f404e77cdcdccb1d61ab189e8e9ef70fd3
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-05 01:13:01 -0700

    Form is now working as expected but doesnt have Validation yet.

--- lib/pages/possible_crime_form.dart
+++ lib/pages/possible_crime_form.dart
@@ -1,46 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:neighborhood_watch_walnut/models/marker_data.dart';
 
 class CriminalFormSubmission extends StatefulWidget {
-  final String markerCoordinates;
+  final double longituteCoordinate;
+  final double latitudeCoordinate;
 
-  CriminalFormSubmission({Key? key, required this.markerCoordinates});
+  CriminalFormSubmission(
+      {Key? key,
+      required this.longituteCoordinate,
+      required this.latitudeCoordinate});
 
   @override
-  CriminalFormSubmissionState createState() {
-    return CriminalFormSubmissionState();
-  }
+  State<CriminalFormSubmission> createState() => _CriminalFormSubmissionState();
 }
 
-class CriminalFormSubmissionState extends State<CriminalFormSubmission> {
+class _CriminalFormSubmissionState extends State<CriminalFormSubmission> {
   final _formKey = GlobalKey<FormState>();
+  final crimeLonController = TextEditingController();
+  final crimeLatController = TextEditingController();
+  final crimeDescriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    crimeDescriptionController.dispose();
+    crimeLatController.dispose();
+    crimeLonController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    crimeLonController.text = widget.longituteCoordinate.toString();
+    crimeLatController.text = widget.latitudeCoordinate.toString();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-        color: Colors.deepOrange,
-        child: Form(
+    return Center(
+      child: Card(
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Form(
             key: _formKey,
             child: Column(
               children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(labelText: "Location"),
-                  readOnly: true,
-                  initialValue: widget.markerCoordinates,
-                ),
-                TextFormField(
-                  maxLines: 10,
-                  decoration:
-                      const InputDecoration(labelText: "Description of Event"),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
+                SizedBox(
+                    width: 250,
+                    child: TextFormField(
+                      controller: crimeLatController,
+                      decoration: const InputDecoration(
+                          labelText: "Latitude Coordinate"),
+
+                      /*
+                      decoration: const InputDecoration(
+                          labelText: "Latitude Coordinate"),
+                      readOnly: true,
+                      initialValue: widget.latitudeCoordinate.toString(),
+                    )*/
+                    )),
+                SizedBox(
+                    width: 250,
+                    child: TextFormField(
+                      controller: crimeLonController,
+                      decoration: const InputDecoration(
+                          labelText: "Longitude Coordinate"),
+                    )),
+                SizedBox(
+                    width: 250,
+                    child: TextFormField(
+                      controller: crimeDescriptionController,
+                      maxLines: 10,
+                      decoration: const InputDecoration(
+                          labelText: "Description of Event"),
+                      // The validator receives the text that the user has entered.
+                    )),
+                ElevatedButton(
+                  onPressed: () {
+                    MarkerData submittedMarker = MarkerData(
+                        lon: double.parse(crimeLonController.text),
+                        lat: double.parse(crimeLatController.text),
+                        description: crimeDescriptionController.text);
+                    Navigator.pop(context, submittedMarker);
                   },
+                  child: const Text("submit"),
                 )
               ],
-            )));
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
