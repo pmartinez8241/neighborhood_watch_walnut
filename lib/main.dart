52593bca6e2b98f08b09312059841b46cdbd9de8
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-05 15:37:10 -0700

    Making changes to bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:neighborhood_watch_walnut/pages/home.dart';
 
 void main() {
@@ -20,11 +21,18 @@ class MainApp extends StatelessWidget {
                   backgroundColor: Colors.orange,
                 )),
             body: const HomePage(),
-            bottomNavigationBar:
-                BottomNavigationBar(items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.camera), label: "Camera"),
-              BottomNavigationBarItem(icon: Icon(Icons.money), label: "Money")
-            ])));
+            bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.money),
+                  label: "Money",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.close),
+                  label: "Close App",
+                ),
+              ],
+              onTap: (value) => {if (value == 1) {}},
+            )));
   }
 }
