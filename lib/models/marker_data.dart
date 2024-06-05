c4cb9c71db5484c9dbfc79e2bd2f0713b053614f
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-05 15:36:22 -0700

    Form returns Date and time

--- lib/models/marker_data.dart
+++ lib/models/marker_data.dart
@@ -1,16 +1,34 @@
+import 'package:intl/intl.dart';
+
 class MarkerData {
-  double? longitude;
-  double? latitude;
-  String? description;
+  double longitude = 0.0;
+  double latitude = 0.0;
+  String description = "";
+  DateTime currentDateTime = DateTime.now();
 
   MarkerData({required double lon, required double lat, required description}) {
     longitude = lon;
     latitude = lat;
     this.description = description;
   }
+
+  String getTime() {
+    final currentTime = DateFormat("HH:mm:ss");
+    final formmattedTime = currentTime.format(currentDateTime);
+    return formmattedTime;
+  }
+
+  String getDate() {
+    final currentDate = DateFormat("yyyy-MM-dd");
+    final formmattedDate = currentDate.format(currentDateTime);
+    return formmattedDate;
+  }
+
   @override
   String toString() {
+    final time = getTime();
+    final date = getDate();
     // TODO: implement toString
-    return "longitude = $longitude \n Latitude = $latitude \n Description = $description";
+    return "longitude = $longitude\n Latitude = $latitude\n Description = $description\n SubmitTime = $time \nSubmit Date = $date";
   }
 }
