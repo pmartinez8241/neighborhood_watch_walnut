e5cd2ac3b35b62696de331cfb4bc38d4f651f299
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-09 20:34:28 -0700

    Added Riverpod ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:neighborhood_watch_walnut/pages/walnut_map.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       initialRoute: "/",
       routes: {"/": (context) => WalnutMap()},
