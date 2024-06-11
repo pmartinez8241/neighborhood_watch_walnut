a649cb28447bb6812ace408cf47c2fd35f9e50d9
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-10 19:33:43 -0700

    Created a list view to see all the markers in the markers list as a list of markers

---
+++ lib/widgets/marker_list_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:neighborhood_watch_walnut/provider/marker_provider.dart';
+
+class MarkersListView extends ConsumerWidget {
+  const MarkersListView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+        body: Stack(children: [
+      (ListView(
+          children: ref
+              .watch(MarkerNotifier.markerNotifierProvier)
+              .map((x) => Card(
+                      child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(x.latitude.toString()),
+                      Text(x.longitude.toString()),
+                      Text(x.id.toString()),
+                      Text(x.description)
+                    ],
+                  )))
+              .toList())),
+      ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Close"))
+    ]));
+  }
+}
