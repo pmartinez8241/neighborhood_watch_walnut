f2fc6f3b6b525ff5842e80ca472ddf69d08277fc
PETE A. MARTINEZ <martinez668@msn.com>
2024-06-13 14:24:18 -0700

    Fix issues with the List View Layout

--- lib/widgets/marker_list_view.dart
+++ lib/widgets/marker_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:neighborhood_watch_walnut/provider/map_controller_provider.dart';
 import 'package:neighborhood_watch_walnut/provider/marker_provider.dart';
 
 class MarkersListView extends ConsumerWidget {
@@ -8,21 +10,33 @@ class MarkersListView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-        body: Stack(children: [
-      (ListView(
-          children: ref
-              .watch(markerNotifierProvider)
-              .map((x) => Card(
-                      child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(x.latitude.toString()),
-                      Text(x.longitude.toString()),
-                      Text(x.id),
-                      Text(x.description)
-                    ],
-                  )))
-              .toList())),
+        body: Column(children: [
+      Expanded(
+          child: ListView(
+              children: ref
+                  .watch(markerNotifierProvider)
+                  .map(
+                    (x) => GestureDetector(
+                      onTap: () {
+                        ref
+                            .watch(mapControllerProviderProvider)
+                            .move(LatLng(x.latitude, x.longitude), 18);
+                        Navigator.pop(context);
+                      },
+                      child: Card(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(x.latitude.toString()),
+                            Text(x.longitude.toString()),
+                            Text(x.id),
+                            Text(x.description)
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList())),
       ElevatedButton(
           onPressed: () {
             Navigator.pop(context);
